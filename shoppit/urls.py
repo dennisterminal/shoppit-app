6d7973a7cbcc6de69edb25366aca23ac8058c706
Dennis <dennomach@gmail.com>
2026-02-05 09:17:46 +0300

    Fix production configuration

--- shoppit/urls.py
+++ shoppit/urls.py
@@ -18,13 +18,17 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+def health_check(request):
+    return JsonResponse({"status": "healthy", "service": "shoppit-backend"})
 
 urlpatterns = [
+    path('api/health/', health_check, name='health_check'),
     path('admin/', admin.site.urls),
     path('', include('shop_app.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
